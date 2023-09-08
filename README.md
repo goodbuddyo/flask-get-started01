52b1397d59e9438016257baa99cec95fbfadf78c
Seano <sean01234@yahoo.com>
2023-09-07 20:01:44 -0400

    start tutorial

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # flask-get-started01
 Flask tutorial
+
+[install python]
+create new project folder
+cd to folder
+python -m venv venv
+source venv/bin/activate
+(venv) $ python -m pip install flask
+(venv) $ export FLASK_APP=flashcards.py
+(venv) $ export FLASK_ENV=developement
+(venv) $ flask run
+http://127.0.0.1:5000
---
+++ flashcards.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, abort, jsonify
+
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def welcome():
+    return 'Welcome to my app'
+
+
+@app.route("/date")
+def date():
+    return "This page was served at: " + str(datetime.now())
+
+
+counter = 0
+
+
+@app.route('/count_views')
+def count_demo():
+    global counter
+    counter += 1
+    return "This page was served " + str(counter) + " times."
