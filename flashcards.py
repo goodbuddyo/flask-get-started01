15e6993854d1c35916f9ef440ebe698667436867
Seano <sean01234@yahoo.com>
2023-09-09 21:38:51 -0400

    minor changes {{ message }} test

--- flashcards.py
+++ flashcards.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, abort, jsonify
+# from datetime import datetime
 
-from datetime import datetime
+from model import db
 
 app = Flask(__name__)
 
@@ -9,19 +10,33 @@ app = Flask(__name__)
 def welcome():
     return render_template(
         'welcome.html',
-        message="Here's a message from the view")
+        message="Here's a message from the view!!")
 
 
-@app.route("/date")
-def date():
-    return "This page was served at: " + str(datetime.now())
+@app.route('/card/<int:index>')
+def card_view(index):
+    try:
+        card = db[index]
+        return render_template("card.html", card=card, index=index, max_index=len(db)-1)
+    except IndexError:
+        abort(404)
 
 
-counter = 0
+@app.route("/api/card/")
+def api_card_list():
+    return jsonify(db)
 
 
-@app.route('/count_views')
-def count_demo():
-    global counter
-    counter += 1
-    return "This page was served " + str(counter) + " times."
+# @app.route("/date")
+# def date():
+#    return "This page was served at: " + str(datetime.now())
+
+
+# counter = 0
+
+
+# @app.route('/count_views')
+# def count_demo():
+#    global counter
+#    counter += 1
+#    return "This page was served " + str(counter) + " times."
